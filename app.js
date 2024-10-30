12502572f746f874a15b5ed13531eff463a613ef
Ravindupereraofficial <ravindurandika2004@gmail.com>
2024-10-30 09:58:24 +0530

    new update

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+let randomnumber= (Math.floor((Math.random()*10+1)));
+console.log(randomnumber)
+
 function guess(){
 
-  let randomnumber= (Math.floor((Math.random()*10+1)));
-  console.log(randomnumber)
-
+  
   let userinput=document.getElementById("catchnumber").value;
   console.log(userinput)
 
   if(randomnumber==userinput){
-    document.write("")
+    chancesleft.innerHTML = "You Won The Game";
+    attempt.innerHTML = "your Guessing Number Is Correct";
+  } else{
+
+    if(randomnumber>userinput){
+        coolorhot.innerHTML="your Number Is low"
+   
+    }else if (randomnumber>userinput){
+        coolorhot.innerHTML="your Number Is Higher"
+    }
+
+        
+
+    }
+
+
+
   }
 
-
-}
